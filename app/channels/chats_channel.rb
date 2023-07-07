09896097ae619c7f8324c7917edbdd8911eff948
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-07-07 16:06:13 +0630

    create the channel

--- app/channels/chats_channel.rb
+++ app/channels/chats_channel.rb
@@ -1,6 +1,7 @@
 class ChatsChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stop_all_streams 
+    stream_from "chat_#{params[:user_id]}#{params[:recipient_id]}" 
   end
 
   def unsubscribed
